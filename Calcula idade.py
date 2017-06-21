2326ee74a87b2d6da2e4672565dab985a3bba28c
danielquelis <danielquelis@icloud.com>
2017-06-21 19:47:27 -0300

    Update and rename calcula idade.py to Calcula idade.py

--- Calcula idade.py
+++ Calcula idade.py
@@ -1,10 +1,10 @@
+#Programa lê data de nascimento do usuário, insere num vetor e calcula a idade baseado na data atual.
 data = input("Informe a data de nascimento [dd/mm/aaaa] -> ")
-# print(len(data))
 d = int(data[0:2])
 m = int(data[3:5])
 a = int(data[6:10])
-ma = 3
-da = 15
+ma = 6
+da = 21
 i = (2017 - a)
 if ma < m:
     print(i - 1)
