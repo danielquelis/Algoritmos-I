58bc8e6c58c140047ef92f62c43d50e2d8fc1e52
danielquelis <danielquelis@icloud.com>
2017-06-21 20:48:41 -0300

    Update and rename conversor ºF para ºC.py to Conversor ºF para ºC.py

--- Conversor ºF para ºC.py
+++ Conversor ºF para ºC.py
@@ -1,3 +1,4 @@
+#Programa lê temperatura em Fahrenheit e converte para Celsius
 print("Conversor de temperatura Fahrenheit para Celsius")
 f=float(input("Insira a temperatura em Fahrenheit: "))
 c=(f)-32
