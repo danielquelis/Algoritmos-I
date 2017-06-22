32fd5a2502606f1a0192e539d2a7baef83d07f63
danielquelis <danielquelis@icloud.com>
2017-06-21 21:34:51 -0300

    Add files via upload

---
+++ Matriz.py
@@ -0,0 +1,6 @@
+#Programa lê nº de linhas e colunas que usuário deseja e insere valor 0 em cada posição
+l=int(input('Digite o número de linhas da matriz: '))
+c=int(input('Digite o número de colunas da matriz: '))
+p=[0]*c
+mat=[p]*l
+print(mat)
