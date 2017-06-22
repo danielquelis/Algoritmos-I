ac07c292933c23198ca45dcc4b253722d89cb41e
danielquelis <danielquelis@icloud.com>
2017-06-21 21:16:56 -0300

    Add files via upload

---
+++ Área de pintura.py
@@ -0,0 +1,11 @@
+a=int(input("Digite a área a ser pintada: "))
+l=a//54
+if a%54!=0:
+    l=l+1
+    v=l*80
+    print("Você precisa de %d latas"%l)
+    print("Custa %d reais"%v)
+else:
+    v=l*80
+    print("Você precisa de %d latas"%l)
+    print("custa %d reais"%v)
